d28c29c96839688aa84ef79c0ec83d5a673cebba
Moritz Rocholl <moritz.rocholl@yahoo.de>
2017-11-21 22:15:18 +0100

    interim

--- pyMentalModels/reasoner/numpy_reasoner.py
+++ pyMentalModels/reasoner/numpy_reasoner.py
@@ -3,7 +3,7 @@
 
 import numpy as np
 from itertools import product, combinations
-from sympy.logic.boolalg import (And, Or, Xor, Not,)
+from sympy.logic.boolalg import (And, Or, Xor, Not, Implies, Equivalent)
 from sympy.core.symbol import Symbol
 
 #######################################################################
@@ -25,7 +25,7 @@ is allowed to be false or not represented
 The implementation here for `Or` first adds each model to the merged_model
 and adds all the models together like the `And` operator does.
 Hence, the `OR` operator acts quite like a combination of both the `Xor`
-and And operator for all submodels piecewise and total.
+and And operator for all subexpressions piecewise and total.
     For (A & B) or (C & D) or (E xor F)
     with (A & B):= Arg1, (C & D):=2, etc...
 
@@ -61,19 +61,40 @@ Examples of this behaviour would be:
 Hence, an explicit `Not` in the expression will lead to an explicit `Not`
 representation in the final model.
 There seems to be a big difference in the semantic importance between the
-negation of an atom and the negation of a submodel.
+negation of an atom and the negation of a subexpression.
 
 
 """
 
 
-def builder(sympified_expr):
+def mental_model_builder(sympified_expr):
     """
-    Builds a mental model representation of the logical expression
+    Builds a mental model representation of the logical expression.
+
+    A Mental model is a mental representation of a logical or indeed any expression.
+    An example would be the expression:
+
+        You have either the salad or the soup or the bread
+
+    The mental model representation would then be:
+
+                    Salad
+                            Soup
+                                    Bread
+
+    `mental_model_builder` recursively builds models of the subexpressions of
+    the total expression, merges them and returns the overall mental model
+    representation of the expression
 
     Parameters
     ----------
     sympified_expr: sympy BooleanFunction
+        An expression formatted and processed by the `sympy` python module
+        Attributes:
+            expression.atoms
+                Set of all the atoms in the logical expression
+            expression.args
+                Tuple of arguments the outermost logical operator takes
 
     Returns
     -------
@@ -83,229 +104,215 @@ def builder(sympified_expr):
     # Extract atoms from sympified expression
     exp_atoms = sympified_expr.atoms()
 
-    # mop every atom to its corresponding index in the model
-    var_ind_map = {atom: i for i, atom in enumerate(sorted(exp_atoms, key=str))}
+    # map every atom to its corresponding index in the model
+    atom_index_mapping = {atom: i for i, atom in enumerate(sorted(exp_atoms, key=str))}
 
-    def _merge_models(*sub_models, op):
-        """
-        Merges the different submodels together.
-        This is a momentary template for the other merging functions
-        Implements merging for operator `and`
+    return map_instance_to_operation(sympified_expr)(sympified_expr, atom_index_mapping, exp_atoms)
 
-        Parameters
-        ----------
-        sub_models: List of arbitrary #sub_models
-            submodels to be merged together
-        """
-        print("Merging models")
-        assert(len(sub_models)) >= 2
 
-        if op == "And":
-            iter_models = iter(sub_models)
-            merged_models = next(iter_models)
-            while True:
-                try:
-                    model2 = next(iter_models)
-                except StopIteration:
-                    break
-                reshaped_merged_models = np.repeat(merged_models, len(model2), axis=0)
-                reshaped_model2 = np.tile(model2, (len(merged_models), 1))
-                merged_models = reshaped_merged_models + reshaped_model2
-            return merged_models
-        if op == "Xor":
-            iter_models = iter(sub_models)
-            merged_models = next(iter_models)
-            while True:
-                try:
-                    model2 = next(iter_models)
-                except StopIteration:
-                    break
-                merged_models = np.vstack((merged_models, model2))
-            return merged_models
+def _merge_models(*sub_models, op):
+    """
+    Merges the different subexpressions together.
+    Implements merging for operator `And`, `Or`, `Xor`
 
-        if op == "Or":
-            # first xor everything
-            xor_models = _merge_models(*sub_models, op="Xor")
+    Parameters
+    ----------
+    sub_models: List of arbitrary number of sub_models
+        subexpressions to be merged together
+    """
+    assert(len(sub_models)) >= 2
 
-            merged_models = xor_models
-            # then piecewise and everything
-            list_of_piecewise_ands = [
-                _merge_models(*comb, op="And")
-                for comb in combinations(sub_models, 2)
-            ]
-            for el in list_of_piecewise_ands:
-                merged_models = np.vstack((merged_models, el))
+    if op == "And":
+        iter_models = iter(sub_models)
+        merged_models = next(iter_models)
+        for model in iter_models:
+            reshaped_merged_models = np.repeat(merged_models, len(model), axis=0)
+            reshaped_model2 = np.tile(model, (len(merged_models), 1))
+            merged_models = reshaped_merged_models + reshaped_model2
+        return merged_models
 
-            # then total and everything
-            and_everything = _merge_models(*sub_models, op="And")
+    if op == "Xor":
+        iter_models = iter(sub_models)
+        merged_models = next(iter_models)
+        for model in iter_models:
+            merged_models = np.vstack((merged_models, model))
+        return merged_models
 
-            merged_models = np.vstack((merged_models, and_everything))
+    if op == "Or":
+        # first xor everything
+        xor_models = _merge_models(*sub_models, op="Xor")
 
-            return merged_models
+        merged_models = xor_models
+        # then piecewise and everything
+        list_of_piecewise_ands = [
+            _merge_models(*comb, op="And")
+            for comb in combinations(sub_models, 2)
+        ]
+        for el in list_of_piecewise_ands:
+            merged_models = np.vstack((merged_models, el))
 
-    def map_instance_to_operation(el):
-        "maps every instance to its builder function."
-        maps = iter((
-            (Or, build_or),
-            (And, build_and),
-            (Xor, build_xor),
-            (Not, build_not),
-        ))
-        try:
-            return next(builder for type_, builder in maps if isinstance(el, type_))
-        except StopIteration:
-            raise ValueError("Not a valid operator")
+        # then total and everything
+        and_everything = _merge_models(*sub_models, op="And")
 
-    def build_or(exp):
+        merged_models = np.vstack((merged_models, and_everything))
 
-        assert(isinstance(exp, Or))
+        return merged_models
 
-        or_args = exp.args
-        nr_args = len(or_args)
 
-        if all(isinstance(el, Symbol) for el in exp.args):
-            pos_valuations = [x for x in product(range(2), repeat=nr_args)][1:]
-            pos_valuations = sorted(pos_valuations, key=_increasing_ones_first_sort)
-            or_model = np.empty((len(pos_valuations), len(exp_atoms)))
-            or_model[
-                :, list(map(lambda x: var_ind_map[x], or_args))
-            ] = pos_valuations
-            return or_model
-        else:
-            symbol_list = []
-            submodel_list = []
-            for el in exp.args:
-                if isinstance(el, Symbol):
-                    symbol_list.append(el)
-                else:
-                    submodel_list.append(el)
+def map_instance_to_operation(el):
+    "maps every instance to its builder function."
+    maps = iter((
+        (Or, build_or),
+        (And, build_and),
+        (Xor, build_xor),
+        (Not, build_not),
+        (Implies, build_and),
+        (Equivalent, build_and),
+    ))
+    try:
+        return next(builder for type_, builder in maps if isinstance(el, type_))
+    except StopIteration:
+        raise ValueError("Not a valid operator")
 
-            # Create `or` model for the symbols
+
+def build_or(exp, atom_index_mapping, exp_atoms):
+
+    assert(isinstance(exp, Or))
+
+    or_args = exp.args
+    nr_args = len(or_args)
+
+    if all(isinstance(el, Symbol) for el in exp.args):
+        pos_valuations = [x for x in product(range(2), repeat=nr_args)][1:]
+        pos_valuations = sorted(pos_valuations, key=_increasing_ones_first_sort)
+        or_model = np.zeros((len(pos_valuations), len(exp_atoms)))
+        or_model[
+            :, list(map(lambda x: atom_index_mapping[x], or_args))
+        ] = pos_valuations
+        return or_model
+    else:
+        symbol_list = []
+        subexpression_list = []
+        for el in exp.args:
+            if isinstance(el, Symbol):
+                symbol_list.append(el)
+            else:
+                subexpression_list.append(el)
+
+        # generate submodels from subexpressions
+        modelized_subexpressions = [
+            map_instance_to_operation(subexpression)(subexpression, atom_index_mapping, exp_atoms)
+            for subexpression in subexpression_list
+        ]
+
+        # Create `or` model for the symbols
+        if symbol_list:
             pos_valuations = [x for x in product(range(2), repeat=len(symbol_list))][1:]
             pos_valuations = sorted(pos_valuations, key=_increasing_ones_first_sort)
-            or_model = np.empty((len(pos_valuations), len(exp_atoms)))
+            or_model = np.zeros((len(pos_valuations), len(exp_atoms)))
             or_model[
-                :, list(map(lambda x: var_ind_map[x], symbol_list))
+                :, list(map(lambda x: atom_index_mapping[x], symbol_list))
             ] = pos_valuations
-            # create submodels for every submodel in submodel_list
-            modelized_submodels = [map_instance_to_operation(submodel)(submodel) for submodel in submodel_list]
-            modelized_submodels.append(or_model)
-            merged_sub_models = _merge_models(*modelized_submodels, op="Or")
-            return merged_sub_models
+            modelized_subexpressions.append(or_model)
 
-    def build_and(exp):
+        # merge the generated submodels to an overall model of `And`
+        merged_sub_models = _merge_models(*modelized_subexpressions, op="Or")
+        return merged_sub_models
 
-        assert(isinstance(exp, And))
 
-        and_args = exp.args
+def build_and(exp, atom_index_mapping, exp_atoms):
 
-        if all(isinstance(el, Symbol) for el in and_args):
-            and_model = np.empty((1, len(exp_atoms)))
+    assert(isinstance(exp, (And, Implies, Equivalent)))
+
+    and_args = exp.args
+
+    if all(isinstance(el, Symbol) for el in and_args):
+        and_model = np.zeros((1, len(exp_atoms)))
+        and_model[
+            :, list(map(lambda x: atom_index_mapping[x], and_args))
+        ] = 1.
+        return and_model
+    else:
+        symbol_list = []
+        subexpression_list = []
+        for el in exp.args:
+            if isinstance(el, Symbol):
+                symbol_list.append(el)
+            else:
+                subexpression_list.append(el)
+        # generate submodels from subexpressions
+        modelized_subexpressions = [
+            map_instance_to_operation(subexpression)(subexpression, atom_index_mapping, exp_atoms)
+            for subexpression in subexpression_list
+        ]
+        # Create `and` model for the symbols
+        if symbol_list:
+            and_model = np.zeros((1, len(exp_atoms)))
             and_model[
-                :, list(map(lambda x: var_ind_map[x], and_args))
+                :, list(map(lambda x: atom_index_mapping[x], symbol_list))
             ] = 1.
-            return and_model
-        else:
-            symbol_list = []
-            submodel_list = []
-            for el in exp.args:
-                if isinstance(el, Symbol):
-                    symbol_list.append(el)
-                else:
-                    submodel_list.append(el)
+            modelized_subexpressions.append(and_model)
 
-            # Create `and` model for the symbols
-            and_model = np.empty((1, len(exp_atoms)))
-            and_model[
-                :, list(map(lambda x: var_ind_map[x], symbol_list))
-            ] = 1.
-            # create submodels for every submodel in submodel_list
-            modelized_submodels = [map_instance_to_operation(submodel)(submodel) for submodel in submodel_list]
-            modelized_submodels.append(and_model)
-            merged_sub_models = _merge_models(*modelized_submodels, op="And")
-            return merged_sub_models
+        # merge the generated submodels to an overall model of `And`
+        merged_sub_models = _merge_models(*modelized_subexpressions, op="And")
+        return merged_sub_models
 
-    def build_xor(exp):
 
-        assert(isinstance(exp, Xor))
+def build_xor(exp, atom_index_mapping, exp_atoms):
 
-        xor_args = exp.args
-        nr_args = len(xor_args)
+    assert(isinstance(exp, Xor))
 
-        if all(isinstance(el, Symbol) for el in exp.args):
-            xor_model = np.empty((nr_args, len(exp_atoms)))
+    xor_args = exp.args
+    nr_xor_args = len(xor_args)
+
+    if all(isinstance(el, Symbol) for el in exp.args):
+        xor_model = np.zeros((nr_xor_args, len(exp_atoms)))
+        xor_model[
+            :, list(map(lambda x: atom_index_mapping[x], xor_args))
+        ] = np.eye(nr_xor_args)
+        return xor_model
+    else:
+        symbol_list = []
+        subexpression_list = []
+        for el in exp.args:
+            if isinstance(el, Symbol):
+                symbol_list.append(el)
+            else:
+                subexpression_list.append(el)
+
+        modelized_subexpressions = [
+            map_instance_to_operation(subexpression)(subexpression, atom_index_mapping, exp_atoms)
+            for subexpression in subexpression_list
+        ]
+
+        if symbol_list:
+            xor_model = np.zeros((len(symbol_list), len(exp_atoms)))
             xor_model[
-                :, list(map(lambda x: var_ind_map[x], xor_args))
-            ] = np.eye(nr_args)
-            return xor_model
-        else:
-            symbol_list = []
-            submodel_list = []
-            for el in exp.args:
-                if isinstance(el, Symbol):
-                    symbol_list.append(el)
-                else:
-                    submodel_list.append(el)
-            modelized_submodels = [map_instance_to_operation(submodel)(submodel) for submodel in submodel_list]
+                :, list(map(lambda x: atom_index_mapping[x], symbol_list))
+            ] = np.eye(len(symbol_list))
+            modelized_subexpressions.append(xor_model)
 
-            if symbol_list:
-                xor_model = np.empty((symbol_list, len(exp_atoms)))
-                xor_model[
-                    :, list(map(lambda x: var_ind_map[x], symbol_list))
-                ] = np.eye(len(symbol_list))
-                modelized_submodels.append(xor_model)
-
-            # Create `or` model for the symbols
-            merged_sub_models = _merge_models(*modelized_submodels, op="Xor")
-            return merged_sub_models
-
-    def build_not(exp):
-        arg = exp.args[0]
-        print(arg)
-        assert isinstance(exp, Not)
-        if isinstance(arg, Symbol):
-            not_model = np.empty((1, len(exp_atoms)))
-            not_model[
-                :, var_ind_map[arg]
-            ] = -1.
-            return not_model
-        else:
-            model_positive = map_instance_to_operation(arg)(arg)
-            print(model_positive)
-            raise NotImplementedError(
-                " Did not implement Negation of sub-expression yet. "
-            )
-
-    def _increasing_ones_first_sort(array_slice):
-                    pos_of_ones = [-array_slice[i] for i, _ in enumerate(array_slice)]
-                    return array_slice.count(1), pos_of_ones
-
-    return map_instance_to_operation(sympified_expr)(sympified_expr)
+        # Create `xor` model for the symbols
+        merged_sub_models = _merge_models(*modelized_subexpressions, op="Xor")
+        return merged_sub_models
 
 
-#######################################################################
-#              Stuff that i do not need but keep anyways              #
-#######################################################################
+def build_not(exp, atom_index_mapping, exp_atoms):
+    neg_arg = exp.args[0]
+    assert isinstance(exp, Not)
+    if isinstance(neg_arg, Symbol):
+        not_model = np.zeros([1, len(exp_atoms)])
+        not_model[
+            0, atom_index_mapping[neg_arg]
+        ] = -1.
+        return not_model
+    else:
+        model_positive = map_instance_to_operation(neg_arg)(neg_arg, atom_index_mapping, exp_atoms)
+        raise NotImplementedError(
+            " Did not implement Negation of sub-expression yet. "
+        )
 
-# XXX The following should never happen. Input should never be an array
-# if isinstance(exp, np.ndarray):
-#     and_array = np.ones((len(exp), len(exp[0]) + 1))
-#     and_array[:, :-1] = exp
-#     return and_array
 
-# XXX Stuff from Xor
-# if all(isinstance(el, Symbol) for el in arguments):
-#             return np.ones(len(exp.atoms()))
-#
-#         # Check if everything is a Symbol
-#         for el in arguments:
-#             if isinstance(el, Not):
-#                 seen_not_list.append(el)
-#                 return build_not(el)
-#             elif not isinstance(el, Symbol):
-#                 return map_instance_to_operation(el)(el)
-#             else:
-#                 pass
-#         if any(isinstance(el, Not) for el in arguments):
-#             return "Witnessed a Not at this place"
-#
+def _increasing_ones_first_sort(array_slice):
+                pos_of_ones = [-array_slice[i] for i, _ in enumerate(array_slice)]
+                return array_slice.count(1), pos_of_ones
