bcd94729010010e8d714b90a540fd25d9f4d87c1
Moritz Rocholl <moritz.rocholl@yahoo.de>
2018-01-16 17:04:17 +0100

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from os.path import realpath, dirname, join as path_join
+from setuptools import setup, find_packages
+
+NAME = "pyMentalModels"
+DESCRIPTION = "pyMentalModels"
+LONG_DESCRIPTION = "pyMentalModels is a Python implementation of the Mental Model reasoning theory."
+MAINTAINER = "Moritz Rocholl"
+MAINTAINER_EMAIL = "moritz.rocholl@gmail.com"
+URL = "https://gkigit.informatik.uni-freiburg.de/coco/mentalmodels.git"
+# license = ??
+VERSION = "0.0.1"
+
+PROJECT_ROOT = dirname(realpath(__file__))
+REQUIREMENTS_FILE = path_join(PROJECT_ROOT, "requirements.txt")
+
+with open(REQUIREMENTS_FILE, "r") as f:
+    INSTALL_REQUIREMENTS = f.read().splitlines()
+
+SETUP_REQUIREMENTS = ["pytest-runner"]
+TEST_REQUIREMENTS = ["pytest", "pytest-cov"]
+
+
+if __name__ == "__main__":
+    setup(
+        name=NAME,
+        version=VERSION,
+        maintainer=MAINTAINER,
+        maintainer_email=MAINTAINER_EMAIL,
+        description=DESCRIPTION,
+        url=URL,
+        long_description=LONG_DESCRIPTION,
+        packages=find_packages(),
+        package_data={"docs": ["*"]},
+        include_package_data=True,
+        install_requires=INSTALL_REQUIREMENTS,
+        setup_requires=SETUP_REQUIREMENTS,
+        tests_require=TEST_REQUIREMENTS,
+)
